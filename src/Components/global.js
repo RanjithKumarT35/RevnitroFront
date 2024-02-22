1507233cb64fe0b6817ec578097930688679eb18
RanjithKumarT35 <51657707+RanjithKumarT35@users.noreply.github.com>
2024-02-22 18:40:07 +0530

    Update global.js

--- src/Components/global.js
+++ src/Components/global.js
@@ -1,4 +1,4 @@
 //const API_URL = "https://forumback.onrender.com";
 //const API_URL = "https://testforumback.onrender.com";
-const API_URL = "http://localhost:4000";
+const API_URL = "http://forumbackend.revnitro.com";
 export default API_URL;
